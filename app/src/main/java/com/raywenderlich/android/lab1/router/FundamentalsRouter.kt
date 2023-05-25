26a23575791074ce20636393ddbdc3efdb7a07ec
Danik212 <125592619+Danik212@users.noreply.github.com>
2023-05-25 17:41:40 +0300

    FundamentalsRouter.kt

--- app/src/main/java/com/raywenderlich/android/lab1/router/FundamentalsRouter.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/router/FundamentalsRouter.kt
@@ -4,9 +4,9 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 
 /**
- * Класс, определяющий экраны, которые есть в нашем приложении.
- *
- * Эти объекты должны соответствовать файлам, которые есть в пакете screens
+
+Класс, определяющий экраны, которые есть в нашем приложении.*
+Эти объекты должны соответствовать файлам, которые есть в пакете screens
  */
 sealed class Screen {
     object Navigation : Screen()
@@ -15,6 +15,11 @@ sealed class Screen {
     object Buttons : Screen()
     object ProgressIndicator : Screen()
     object AlertDialog : Screen()
+    object Row : Screen()
+    object Column : Screen()
+    object Box : Screen()
+    object Surface : Screen()
+    object Scaffold : Screen()
 }
 
 object FundamentalsRouter {
